bc65ef90f4e3b6e9db5e7217da639c54821eef49
rakesh <thotarakesh28@gmail.com>
2023-11-08 08:02:47 +0000

    solution

---
+++ src/main/java/com/example/recipe/Recipe.java
@@ -0,0 +1,50 @@
+package com.example.recipe;
+
+import java.util.List;
+
+public class Recipe {
+    private int recipeId;
+    private String recipeName;
+    private String recipeType;
+    private String[] ingredients;
+
+    public Recipe(int recipeId, String recipeName, String recipeType, String[] ingredients) {
+        this.recipeId = recipeId;
+        this.recipeName = recipeName;
+        this.recipeType = recipeType;
+        this.ingredients = ingredients;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getRecipeType() {
+        return recipeType;
+    }
+
+    public void setRecipeType(String recipeType) {
+        this.recipeType = recipeType;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+}
